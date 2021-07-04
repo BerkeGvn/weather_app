da1558e0824f4714819990364e8e579c0b82f4ff
BerkeGvn <berkegvn@gmail.com>
2021-07-04 23:19:41 +0300

    fix

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,5 +1,5 @@
 async function weather(location = 'london') {
-  const ap = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=7234534582a9cee42e342af67c53a4f9`);
+  const ap = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=7234534582a9cee42e342af67c53a4f9`);
   const response = await ap.json();
   const { name, coord } = response;
   const { country } = response.sys;
